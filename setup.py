0413f4ee04615da1b350da2a258c5784f9137416
Seth Cagampang <seth.cagampang@osnexus.com>
2019-10-22 09:57:38 -0700

    setup files for pypi

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
+setuptools.setup(
+
+     name='QSPyClientLib',  
+     version='1.0',
+     scripts=['qspyclient'],
+     author="Seth Cagampang",
+     author_email="seth.cagampang@osnexus.com",
+     description="QuantaStor REST API python library",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/OSNEXUS/QSPyClient",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )

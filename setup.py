61b76a0c31d8a0c281e87a6f6a3283892b3a7ff4
Seth Cagampang <seth.cagampang@osnexus.com>
2019-10-22 14:03:20 -0700

    fixed interpreter bug

--- setup.py
+++ setup.py
@@ -1,24 +1,23 @@
 import setuptools
 
 with open("README.md", "r") as fh:
-
-    long_description = fh.read()
+    long_description_txt = fh.read()
 
 setuptools.setup(
+    name='quantastor-pkg',  
+    version='0.0.1',
+    scripts=['quantastor/qs_client.py'],
+    author="Seth Cagampang",
+    author_email="seth.cagampang@osnexus.com",
+    description="QuantaStor REST python library",
+    long_description=long_description_txt,
+    long_description_content_type="text/markdown",
+    url="https://github.com/OSNEXUS/QSPyClient",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
 
-     name='quantastor-pkg',  
-     version='0.0.1',
-     scripts=['quantastor/qs_client.py'],
-     author="Seth Cagampang",
-     author_email="seth.cagampang@osnexus.com",
-     description="QuantaStor REST API python library",
-     long_description=long_description,
-     long_description_content_type="text/markdown",
-     url="https://github.com/OSNEXUS/QSPyClient",
-     packages=setuptools.find_packages(),
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: BSD License",
-         "Operating System :: OS Independent",
-     ],
  )

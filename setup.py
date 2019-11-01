0a5beafd17e5e85ffa47905cacc962b238cb79de
Seth Cagampang <seth.cagampang@osnexus.com>
2019-11-01 16:47:49 -0700

    added requests as a requirement

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/OSNEXUS/QSPyClient",
     packages=setuptools.find_packages(),
-    install_requires=['urllib3',],
+    install_requires=['urllib3','requests'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
